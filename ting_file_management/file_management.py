a16687049e9932fb14b5b4b8aff0fc66812cc0a1
danricardo88 <daniel.ricardo88@outlook.com>
2023-05-10 20:30:05 -0300

    req-1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
+import sys
+
+
+def txt_importer(pathfile):
     """Aqui irá sua implementação"""
+    if not pathfile.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(pathfile) as file:
+            textinho = file.read()
+            return textinho.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {pathfile} não encontrado", file=sys.stderr)
