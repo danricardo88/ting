50ff44389421f61142b39b9476ca2d49fad7fb4f
danricardo88 <daniel.ricardo88@outlook.com>
2023-05-10 20:09:53 -0300

    req-1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,17 +2,27 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
-    def __init__(self):
+    def __init__(proprio):
         """Inicialize sua estrutura aqui"""
+        proprio._data = []
 
-    def __len__(self):
+    def __len__(proprio):
         """Aqui irá sua implementação"""
+        return len(proprio._data)
 
-    def enqueue(self, value):
+    def enqueue(proprio, valor):
         """Aqui irá sua implementação"""
+        proprio._data.append(valor)
 
-    def dequeue(self):
+    def dequeue(proprio):
         """Aqui irá sua implementação"""
+        if not proprio._data:
+            raise IndexError("A fila está vazia")
+        return proprio._data.pop(0)
 
-    def search(self, index):
+    def search(proprio, index):
         """Aqui irá sua implementação"""
+        datalength = proprio.__len__()
+        if index < 0 or index > datalength - 1:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return proprio._data[index]
